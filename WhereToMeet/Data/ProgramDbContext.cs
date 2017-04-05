76134f6e5618165c03ba1b8fbf55aa084091e238
Christophine <sharkchris91@gmail.com>
2017-04-06 00:27:13 +0900

    Solution push. Added database.

---
+++ WhereToMeet/Data/ProgramDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WhereToMeet.Data
+{
+    public class ProgramDbContext : DbContext
+    {
+        private const string ConnectionString = "Filename=./WhereToMeet.db";
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(ConnectionString);
+        }
+    }
+}
