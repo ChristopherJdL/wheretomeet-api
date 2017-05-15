4b2f17026a67bc2712fa2c7328774e9ba2e4bca7
Christophine <sharkchris91@gmail.com>
2017-05-15 21:47:05 +0900

    Empty perfect place controller

--- WhereToMeet/Controllers/PerfectPlaceController.cs
+++ WhereToMeet/Controllers/PerfectPlaceController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WhereToMeet.Transporters;
 
 // For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -17,6 +18,29 @@ namespace WhereToMeet.Controllers
             public int[] Participants { get; set; }
         }
         // GET: api/values
+
+        GeoCoordinatesTransporter[] GetParticipantsCoordinates(int[] ParticipantsIds)
+        {
+            return new GeoCoordinatesTransporter [] 
+            {
+                new GeoCoordinatesTransporter()
+                {
+                    X =  126.9960697,
+                    Y = 37.5156865
+                },
+                new GeoCoordinatesTransporter()
+                {
+                    X = 145.9960697,
+                    Y = 40.51004
+                },
+
+                new GeoCoordinatesTransporter()
+                {
+                    X = 150.9960697,
+                    Y = 39.51004
+                }
+            };
+        }
         [HttpGet]
         public IActionResult Get(PerfectPlaceQueryWrapper perfectPlaceQuery)
         {
